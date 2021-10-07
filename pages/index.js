4f55660b329b4fec7f1087caff70b33126dd4af1
zayedibnibrahim <zayed.ibn@gmail.com>
2021-10-08 04:05:32 +0600

    Fixed index ssss issue

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function HomePage({ posts }) {
 
       <Link href='/blog'>
         <a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'>
-          All Postssssss
+          All Posts
         </a>
       </Link>
     </Layout>
